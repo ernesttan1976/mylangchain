5958caa8b24b6e86f6049026f5674767d05470bd
ernesttan1976 <ernesttan1976@gmail.com>
2023-07-04 06:15:15 +0800

    api/logs "GET"

--- pages/api/log.js
+++ pages/api/log.js
@@ -4,18 +4,23 @@ import Log from "../../models/Log"
 
 export default async function handler(req, res) {
 
-    if (req.method !== 'POST') {
+    if (req.method === 'POST') {
+
+        connect();
+
+        const obj = req.body;
+    
+        console.info("req.body", req.body)
+        
+        const log = await Log.create(obj)
+    
+        res.status(200).json({message: "ok"})
+    } else if (req.method === 'GET') {
+        connect();
+        const logs = await Log.find({}).sort({created_at:-1}).limit(10)
+        res.status(200).json({logs: logs})
+    } else {
         res.status(405).json({ message: 'Method Not Allowed' });
         return;
     }
-
-    connect();
-
-    const obj = req.body;
-
-    console.info("req.body", req.body)
-    
-    const log = await Log.create(obj)
-
-    res.status(200).json({message: "ok"})
 }
